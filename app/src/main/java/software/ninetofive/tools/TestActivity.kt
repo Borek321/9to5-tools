7a5d37c2ee9fa23acb3586171a7312d8f620ae74
Borek Beker <borek.beker@9to5.software>
2020-02-25 17:22:49 +0100

    Setup test for photo selector

--- app/src/main/java/software/ninetofive/tools/TestActivity.kt
+++ app/src/main/java/software/ninetofive/tools/TestActivity.kt
@@ -1,6 +1,8 @@
 package software.ninetofive.tools
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import dagger.android.support.DaggerAppCompatActivity
 import software.ninetofive.photoselector.PhotoSelector
@@ -13,6 +15,29 @@ class TestActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val authority = "$packageName.fileprovider"
+        photoSelector.start(this, mapOf(
+            PhotoSelector.Options.FILE_PROVIDER_AUTHORITY_NAME to authority,
+            PhotoSelector.Options.RATIONALE_HANDLER to {
+                photoSelector.allowedRationale()
+            }
+        )) { photo ->
+            Log.e("TestActivity", "Photo taken!")
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        photoSelector.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        photoSelector.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 }
